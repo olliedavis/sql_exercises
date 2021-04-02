407250bf74e4e82b9f4d89d0035f27a1470889b3
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-04-02 22:09:51 +0100

    Sixth Exercise Completed

--- 04_SELECT_within_SELECT.sql
+++ 04_SELECT_within_SELECT.sql
@@ -64,4 +64,11 @@ SELECT name,
 FROM world
 WHERE continent = 'Europe'
 
+#  Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)
 
+
+SELECT name
+FROM world
+WHERE gdp > ALL(SELECT gdp
+                 FROM world
+                 WHERE continent = 'Europe' AND GDP > 1)

2d1b0e02f25dfd7f2ca682f5c6b0ab6582c8fe6f
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-04-02 21:22:55 +0100

    First two Exercises Passed

---
+++ 04_SELECT_within_SELECT.sql
@@ -0,0 +1,32 @@
+-- name	continent	area	population	gdp
+-- Afghanistan	Asia	652230	25500100	20343000000
+-- Albania	Europe	28748	2831741	12960000000
+-- Algeria	Africa	2381741	37100000	188681000000
+-- Andorra	Europe	468	78115	3712000000
+-- Angola	Africa	1246700	20609294	100990000000
+-- ...
+
+
+-- List each country name where the population is larger than that of 'Russia'.
+
+SELECT name
+FROM world
+WHERE population > (
+     SELECT population FROM world
+      WHERE name='russia'
+)
+
+-- Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.
+
+
+SELECT name
+FROM world
+WHERE continent = 'Europe' AND gdp / population > (
+                           SELECT gdp / population
+                           FROM world
+                           WHERE name = 'United Kingdom'
+)
+
+
+
+

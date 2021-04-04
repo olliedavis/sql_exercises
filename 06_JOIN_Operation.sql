0f62a8a979d661c1e523b06e9c10e63d5288fdc4
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-04-04 16:20:56 +0100

    Fifth Exercise Completed

--- 06_JOIN_Operation.sql
+++ 06_JOIN_Operation.sql
@@ -57,3 +57,11 @@ SELECT team1, team2, player
   FROM game
   JOIN goal ON id = matchid
  WHERE player LIKE 'MARIO%'
+
+# Show player, teamid, coach, gtime for all goals scored in the first 10 minutes
+
+SELECT player, teamid, coach, gtime
+  FROM goal
+  JOIN eteam on teamid = id
+ WHERE gtime<=10
+

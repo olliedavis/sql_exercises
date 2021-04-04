7ca823f8813a7bf5419237a50ede2896ae56acb7
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-04-04 18:31:21 +0100

    First Exercise Completed

---
+++ 07_ More JOIN.sql
@@ -0,0 +1,12 @@
+                movie
+id	title	yr	director	budget	gross
+                actor
+              id	name
+                casting
+         movieid  actorid	ord
+# 1.
+# List the films where the yr is 1962 [Show id, title]
+
+SELECT id, title
+  FROM movie
+ WHERE yr=1962

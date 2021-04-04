2397dac0e35322f15b5fb45e2842c6c2c360a720
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-04-04 14:08:15 +0100

    Second Exercise Completed

--- 06_JOIN_Operation.sql
+++ 06_JOIN_Operation.sql
@@ -25,10 +25,17 @@
 
 #1.
 # The first example shows the goal scored by a player with the last name 'Bender'.
-# The * says to list all the columns in the table - a shorter way of saying matchid, teamid, player, gtime
-#
 # Modify it to show the matchid and player name for all goals scored by Germany.
 
 SELECT matchid, player
   FROM goal
  WHERE teamid = 'GER'
+
+# 2.
+# From the previous query you can see that Lars Bender's scored a goal in game 1012.
+# Now we want to know what teams were playing in that match.
+# Show id, stadium, team1, team2 for just game 1012
+
+SELECT id,stadium,team1,team2
+  FROM game
+ WHERE id = 1012

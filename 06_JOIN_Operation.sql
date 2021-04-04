0742428bb3a53becf9d3c1a280a68d57c117c4d9
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-04-04 16:32:44 +0100

    Seventh Exercise Completed

--- 06_JOIN_Operation.sql
+++ 06_JOIN_Operation.sql
@@ -50,24 +50,32 @@ SELECT player, teamid, stadium, mdate
   FROM game
   JOIN goal ON id = matchid
  WHERE teamid = 'ger'
-
+# 4.
 # Show the team1, team2 and player for every goal scored by a player called Mario player LIKE 'Mario%'
 
 SELECT team1, team2, player
   FROM game
   JOIN goal ON id = matchid
  WHERE player LIKE 'MARIO%'
-
+# 5.
 # Show player, teamid, coach, gtime for all goals scored in the first 10 minutes
 
 SELECT player, teamid, coach, gtime
   FROM goal
   JOIN eteam on teamid = id
  WHERE gtime<=10
-
+# 6.
 # List the dates of the matches and the name of the team in which 'Fernando Santos' was the team1 coach.
 
 SELECT mdate, teamname
   FROM game
   JOIN eteam ON team1 = eteam.id
  WHERE coach = 'Fernando Santos'
+
+# 7.
+# List the player for every goal scored in a game where the stadium was 'National Stadium, Warsaw'
+
+SELECT player
+  FROM goal
+  JOIN game ON matchid = id
+ WHERE stadium = 'National Stadium, Warsaw'

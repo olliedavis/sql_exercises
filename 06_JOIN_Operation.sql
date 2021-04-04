490f301446695ba2152ef1658190ac5b73fe2b44
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-04-04 17:02:09 +0100

    Eleventh Exercise Completed

--- 06_JOIN_Operation.sql
+++ 06_JOIN_Operation.sql
@@ -104,3 +104,12 @@ GROUP BY teamname
     JOIN goal ON id=matchid
 GROUP BY stadium
 
+# 11
+# For every match involving 'POL', show the matchid, date and the number of goals scored.
+
+   SELECT matchid, mdate, COUNT(teamid)
+     FROM game
+     JOIN goal ON id = matchid
+    WHERE team1 = 'POL' OR team2 = 'POL'
+ GROUP BY matchid, mdate
+

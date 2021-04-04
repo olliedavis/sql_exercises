faa3f54f7ce6d600e767f9cfa5ea52bde1425eda
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-04-04 16:39:12 +0100

    Eighth Exercise Completed

--- 06_JOIN_Operation.sql
+++ 06_JOIN_Operation.sql
@@ -79,3 +79,11 @@ SELECT player
   FROM goal
   JOIN game ON matchid = id
  WHERE stadium = 'National Stadium, Warsaw'
+
+# 8.
+# Show the name of all players who scored a goal against Germany.
+
+SELECT DISTINCT player
+  FROM goal
+  JOIN game ON matchid = id
+ WHERE (team1 = 'GER' OR team2 = 'GER') AND teamid != 'GER'

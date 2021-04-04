bb8e19bb8ddf06a6ad8984234af21399afa82d51
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-04-04 16:19:02 +0100

    Fourth Exercise Completed

--- 06_JOIN_Operation.sql
+++ 06_JOIN_Operation.sql
@@ -50,3 +50,10 @@ SELECT player, teamid, stadium, mdate
   FROM game
   JOIN goal ON id = matchid
  WHERE teamid = 'ger'
+
+# Show the team1, team2 and player for every goal scored by a player called Mario player LIKE 'Mario%'
+
+SELECT team1, team2, player
+  FROM game
+  JOIN goal ON id = matchid
+ WHERE player LIKE 'MARIO%'

e3b53d4d2732940b8b0290a36767183405cb5298
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-04-04 20:13:56 +0100

    Added Top Comment

--- 07_ More JOIN.sql
+++ 07_ More JOIN.sql
@@ -1,9 +1,9 @@
-                movie
-id	title	yr	director	budget	gross
-                actor
-              id	name
-                casting
-         movieid  actorid	ord
+--                 movie
+-- id	title	yr	director	budget	gross
+--                 actor
+--               id	name
+--                 casting
+--          movieid  actorid	ord
 -- 1.
 -- List the films where the yr is 1962 [Show id, title]
 

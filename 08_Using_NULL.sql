513dc39527255d0f78c82e69173a5b3960011f59
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-04-05 14:37:23 +0100

    Fifth Exercise Completed

--- 08_Using_NULL.sql
+++ 08_Using_NULL.sql
@@ -42,3 +42,9 @@ LEFT JOIN dept ON teacher.dept = dept.id
     SELECT teacher.name, dept.name
       FROM teacher
 RIGHT JOIN dept ON teacher.dept = dept.id
+
+-- 5
+-- Use COALESCE to print the mobile number. Use the number '07986 444 2266' if there is no number given. Show teacher name and mobile number or '07986 444 2266'
+
+SELECT name, COALESCE(mobile, '07986 444 2266') AS mobile
+  FROM teacher

df4210f7f166be68075460e790753e6709b9c76c
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-04-05 17:14:43 +0100

    Sixth Exercise Complete

--- 09_Self_JOIN.sql
+++ 09_Self_JOIN.sql
@@ -40,3 +40,14 @@ SELECT a.company, a.num, a.stop, b.stop
   FROM route a 
   JOIN route b ON (a.company = b.company AND a.num = b.num)
  WHERE a.stop=53 AND b.stop = 149
+
+--  6.
+-- The query shown is similar to the previous one, however by joining two copies of the stops table we can refer to stops by name rather than by number.
+--  Change the query so that the services between 'Craiglockhart' and 'London Road' are shown
+
+SELECT a.company, a.num, stopa.name, stopb.name
+  FROM route a 
+        JOIN route b ON (a.company=b.company AND a.num=b.num)
+        JOIN stops stopa ON (a.stop=stopa.id)
+        JOIN stops stopb ON (b.stop=stopb.id)
+       WHERE stopa.name='Craiglockhart' AND stopb.name = 'London Road'

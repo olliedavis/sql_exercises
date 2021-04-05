2588376e0ce2acdab45fcff449bdcdfac19c410c
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-04-05 14:41:45 +0100

    Sixth Exercise Completed

--- 08_Using_NULL.sql
+++ 08_Using_NULL.sql
@@ -48,3 +48,10 @@ RIGHT JOIN dept ON teacher.dept = dept.id
 
 SELECT name, COALESCE(mobile, '07986 444 2266') AS mobile
   FROM teacher
+
+-- 6
+-- Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. Use the string 'None' where there is no department.
+
+    SELECT teacher.name, COALESCE(dept.name, 'None')
+      FROM teacher
+ LEFT JOIN dept ON dept = dept.id

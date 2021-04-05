7c03a8c1fe734b916497b83b1002789f8ee4737d
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-04-05 14:34:10 +0100

    Fourth Exercise Completed

--- 08_Using_NULL.sql
+++ 08_Using_NULL.sql
@@ -35,3 +35,10 @@ INNER JOIN dept ON teacher.dept = dept.id
    SELECT teacher.name, dept.name
      FROM teacher
 LEFT JOIN dept ON teacher.dept = dept.id
+
+-- 4
+-- Use a different JOIN so that all departments are listed.
+
+    SELECT teacher.name, dept.name
+      FROM teacher
+RIGHT JOIN dept ON teacher.dept = dept.id

513a6753cee36c48e760a4cb1b0ba92cb776d4a8
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-04-05 14:31:25 +0100

    Second Exercise Complete

--- 08_Using_NULL.sql
+++ 08_Using_NULL.sql
@@ -15,9 +15,16 @@
 -- 3	Engineering
 -- ...
 
-
+-- 1.
 -- List the teachers who have NULL for their department.
 
 SELECT name
   FROM teacher
  WHERE dept IS NULL
+
+-- 2
+-- Note the INNER JOIN misses the teachers with no department and the departments with no teacher.
+
+    SELECT teacher.name, dept.name
+      FROM teacher 
+INNER JOIN dept ON teacher.dept = dept.id

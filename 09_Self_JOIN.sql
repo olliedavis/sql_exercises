0fb4eda40870dcd26126ffc61d0c1e6e8e4fe05c
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-04-05 16:59:37 +0100

    Third Exercise Complete

--- 09_Self_JOIN.sql
+++ 09_Self_JOIN.sql
@@ -3,3 +3,19 @@
 
 SELECT COUNT(name)
   FROM stops
+
+  -- 2
+  -- Find the id value for the stop 'Craiglockhart'
+
+SELECT id
+  FROM stops
+ WHERE name = 'Craiglockhart'
+ 
+-- 3
+-- Give the id and the name for the stops on the '4' 'LRT' service.
+
+ SELECT id, name
+   FROM stops
+   JOIN route ON id = stop
+  WHERE route.company = 'LRT'
+    AND num = '4'
